58b8c71c2f30fd0321b2b86fe5965e6cbbe28c12
Robert <72943079+CnC-Robert@users.noreply.github.com>
2022-05-28 20:53:59 +0200

    Add deploy on option

--- build-from-source.sh
+++ build-from-source.sh
@@ -110,9 +110,9 @@ for file in $DIR/revanced-patcher/build/libs/*.jar; do cp "$file" "$DIR/build/re
 cd build
 
 if [ ! -z "$JAVA_HOME" ]; then
-	"$JAVA_HOME/bin/java" -jar "revanced-cli.jar" -a "youtube.apk" -m "integrations.apk" -o "revanced.apk" -p "revanced-patches.jar" -r -t "temp"
+	"$JAVA_HOME/bin/java" -jar "revanced-cli.jar" -a "youtube.apk" $(if [ ! -e "$1" ]; then echo "-d $1"; fi) -m "integrations.apk" -o "revanced.apk" -p "revanced-patches.jar" -r -t "temp"
 else
-	java -jar "revanced-cli.jar" -a "youtube.apk" -m "integrations.apk" -o "revanced.apk" -p "revanced-patches.jar" -r -t "temp"
+	java -jar "revanced-cli.jar" -a "youtube.apk" $(if [ ! -e "$1" ]; then echo "-d $1"; fi) -m "integrations.apk" -o "revanced.apk" -p "revanced-patches.jar" -r -t "temp"
 fi
 
 cp "$DIR/build/revanced.apk" "$DIR/revanced.apk"
