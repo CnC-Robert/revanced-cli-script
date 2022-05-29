6c7ebe1df15b8d730e1c7467c69b2f71e3b77909
Robert <72943079+CnC-Robert@users.noreply.github.com>
2022-05-29 13:22:10 +0200

    Add comments to steps

--- build-from-source.sh
+++ build-from-source.sh
@@ -2,6 +2,7 @@
 
 DIR="$(pwd)"
 
+# Check if java is installed and if not, download and extract openjdk 17
 if [ ! -e "$(which java)" ] && [ -z "$JAVA_HOME" ]; then
 	export JAVA_HOME="$(readlink -f "$DIR/openjdk")"
 	if [ ! -e "$JAVA_HOME/bin/java" ]; then
@@ -15,6 +16,7 @@ if [ ! -e "$(which java)" ] && [ -z "$JAVA_HOME" ]; then
 	fi
 fi
 
+# Check if android sdk is installed and if not, download and extract android sdk
 if [ -z "$ANDROID_HOME" ] && [ -z "$ANDROID_SDK_ROOT" ]; then
 	export ANDROID_HOME="$(readlink -f "$DIR/android-sdk")"
 	if [ ! -e "$ANDROID_HOME" ]; then
@@ -27,6 +29,7 @@ if [ -z "$ANDROID_HOME" ] && [ -z "$ANDROID_SDK_ROOT" ]; then
 	fi
 fi
 
+# Check if a youtube.apk is provided before continuing
 if [ ! -e "$DIR/build/youtube.apk" ]; then
 	echo "Error, ./build/youtube.apk not found"
 	exit 1
@@ -43,20 +46,25 @@ if ! command -v "git" &> "/dev/null"; then
 	exit 1
 fi
 
-
 echo
+
+# Clone the patcher and publish it
 git clone https://github.com/revanced/revanced-patcher
 cd revanced-patcher
 git checkout dev
 chmod +x ./gradlew
 
+# If $LOCALMAVEN is set to 1 use publishToMavenLocal instead of publish
 ./gradlew $(if [ "$LOCALMAVEN" == "1" ]; then echo "publishToMavenLocal"; else echo "publish"; fi)
 
+# If the program did not build & publish correctly exit the script
 if [ ! $? == 0 ]; then exit 1; fi
 
 cd ..
 
 echo
+
+# Clone the patches and publish it
 git clone https://github.com/revanced/revanced-patches
 cd revanced-patches
 git checkout dev
@@ -69,6 +77,8 @@ if [ ! $? == 0 ]; then exit 1; fi
 cd ..
 
 echo
+
+# Clone the cli and build it
 git clone https://github.com/revanced/revanced-cli
 cd revanced-cli
 git checkout dev
@@ -81,6 +91,8 @@ if [ ! $? == 0 ]; then exit 1; fi
 cd ..
 
 echo
+
+# Clone the integrations and build it
 git clone https://github.com/revanced/revanced-integrations
 cd revanced-integrations
 chmod +x ./gradlew
@@ -91,19 +103,24 @@ if [ ! $? == 0 ]; then exit 1; fi
 
 cd ..
 
+# Copy the cli, integrations, patches and patcher to the build directory
 cp revanced-cli/build/libs/revanced-cli-*-all.jar build/revanced-cli.jar
 cp revanced-integrations/app/build/outputs/apk/release/*.apk build/integrations.apk
+
+# Since the cli does not need the -sources.jar & -javadoc.jar the script will copy the all files from the directory and stop after the first, this is bit crappy but it works
 for file in $DIR/revanced-patches/build/libs/*.jar; do cp "$file" "$DIR/build/revanced-patches.jar"; break; done 
 for file in $DIR/revanced-patcher/build/libs/*.jar; do cp "$file" "$DIR/build/revanced-patcher.jar"; break; done
 
 cd build
 
+# Get the correct java executable
 if [ -z "$JAVA_HOME" ]; then
 	JAVA="java"
 else
 	JAVA="$JAVA_HOME/bin/java"
 fi
 
+# Execute the cli and if an adb device name is given deploy on device
 "$JAVA" -jar "revanced-cli.jar" -a "youtube.apk" $(if [ ! -z "$1" ]; then echo "-d $1"; fi) -m "integrations.apk" -o "revanced.apk" -p "revanced-patches.jar" -r -t "temp"
 
 cp "$DIR/build/revanced.apk" "$DIR/revanced.apk"
