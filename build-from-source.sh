1febc69bc0173424eb2dd76fcc8443d7760b53f9
Robert <72943079+CnC-Robert@users.noreply.github.com>
2022-05-29 12:23:49 +0200

    Fix check if argument is empty

--- build-from-source.sh
+++ build-from-source.sh
@@ -104,7 +104,7 @@ else
 	JAVA="$JAVA_HOME/bin/java"
 fi
 
-"$JAVA" -jar "revanced-cli.jar" -a "youtube.apk" $(if [ ! -e "$1" ]; then echo "-d $1"; fi) -m "integrations.apk" -o "revanced.apk" -p "revanced-patches.jar" -r -t "temp"
+"$JAVA" -jar "revanced-cli.jar" -a "youtube.apk" $(if [ ! -z "$1" ]; then echo "-d $1"; fi) -m "integrations.apk" -o "revanced.apk" -p "revanced-patches.jar" -r -t "temp"
 
 cp "$DIR/build/revanced.apk" "$DIR/revanced.apk"
 
